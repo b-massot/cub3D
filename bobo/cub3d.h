6cc66d8b68579c5558ebd536935fcd91723704e4
B-massot <b.massot812@gmail.com>
2026-06-05 09:36:20 +0200

    bobo 1st draft algo in main

--- bobo/cub3d.h
+++ bobo/cub3d.h
@@ -11,25 +11,88 @@
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-# define CUB3D_h
+# define CUB3D_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
+# include <math.h>
 # include "../libft/libft.h"
 # include "../minilibx-linux/mlx.h"
 
-# define TILE_SIZE 100
+# define WIN_W 1024
+# define WIN_H 768
+
 # define ESC 65307
 # define W 119
 # define A 97
 # define S 115
 # define D 100
-# define UP 65362
-# define DOWN 65364
 # define LEFT 65361
 # define RIGHT 65363
 
+# define MOVE_SPEED 0.08
+# define ROT_SPEED 0.05
 
+# define MINIMAP_SCALE 12
+
+typedef struct s_img
+{
+    void	*ptr;
+    char	*addr;
+    int		bpp;
+    int		line_len;
+    int		endian;
+}	t_img;
+
+typedef struct s_map
+{
+    char	**grid;
+    int		width;
+    int		height;
+}	t_map;
+
+typedef struct s_player
+{
+    double	x;
+    double	y;
+    double	dir_x;
+    double	dir_y;
+    double	plane_x;
+    double	plane_y;
+}	t_player;
+
+typedef struct s_ray
+{
+    double	camera_x;
+    double	ray_dir_x;
+    double	ray_dir_y;
+    int		map_x;
+    int		map_y;
+    double	side_dist_x;
+    double	side_dist_y;
+    double	delta_dist_x;
+    double	delta_dist_y;
+    double	perp_wall_dist;
+    int		step_x;
+    int		step_y;
+    int		hit;
+    int		side;
+}	t_ray;
+
+typedef struct s_game
+{
+    void		*mlx;
+    void		*win;
+    t_img		img;
+    t_map		map;
+    t_player	player;
+    int			key_w;
+    int			key_a;
+    int			key_s;
+    int			key_d;
+    int			key_left;
+    int			key_right;
+}	t_game;
 
 #endif
--- bobo/main.c
+++ bobo/main.c
@@ -1,48 +1,316 @@
 #include "cub3d.h"
 
-typedef struct s_app
-{
-	void	*mlx;
-	void	*win;
-	void	*img;
-} 	t_app;
+static char	*g_map_data[] = {
+	"111111111111",
+	"100000000001",
+	"101111100001",
+	"100000100001",
+	"100011100001",
+	"100000000001",
+	"100000000001",
+	"111111111111",
+	NULL
+};
 
-static int	close_window(t_app *app)
+static int	is_wall(t_game *game, int x, int y)
 {
-	if (app->img)
-		mlx_destroy_image(app->mlx, app->img);
-	if (app->win)
-		mlx_destroy_window(app->mlx, app->win);
+	if (x < 0 || y < 0 || y >= game->map.height || x >= game->map.width)
+		return (1);
+	return (game->map.grid[y][x] == '1');
+}
+
+static void	put_pixel(t_img *img, int x, int y, int color)
+{
+	char	*dst;
+
+	if (x < 0 || y < 0 || x >= WIN_W || y >= WIN_H)
+		return ;
+	dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
+	*(unsigned int *)dst = (unsigned int)color;
+}
+
+static void	draw_vertical_line(t_game *game, int x, int start, int end, int color)
+{
+	int	y;
+
+	if (start < 0)
+		start = 0;
+	if (end >= WIN_H)
+		end = WIN_H - 1;
+	y = start;
+	while (y <= end)
+	{
+		put_pixel(&game->img, x, y, color);
+		y++;
+	}
+}
+
+static void	draw_background(t_game *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < WIN_H)
+	{
+		x = 0;
+		while (x < WIN_W)
+		{
+			if (y < WIN_H / 2)
+				put_pixel(&game->img, x, y, 0x87CEEB);
+			else
+				put_pixel(&game->img, x, y, 0x3C2F2F);
+			x++;
+		}
+		y++;
+	}
+}
+
+static void	init_ray(t_game *game, t_ray *ray, int x)
+{
+	ray->camera_x = 2.0 * x / (double)WIN_W - 1.0;
+	ray->ray_dir_x = game->player.dir_x + game->player.plane_x * ray->camera_x;
+	ray->ray_dir_y = game->player.dir_y + game->player.plane_y * ray->camera_x;
+	ray->map_x = (int)game->player.x;
+	ray->map_y = (int)game->player.y;
+	if (ray->ray_dir_x == 0)
+		ray->delta_dist_x = 1e30;
+	else
+		ray->delta_dist_x = fabs(1.0 / ray->ray_dir_x);
+	if (ray->ray_dir_y == 0)
+		ray->delta_dist_y = 1e30;
+	else
+		ray->delta_dist_y = fabs(1.0 / ray->ray_dir_y);
+	ray->hit = 0;
+}
+
+static void	set_steps_and_sidedist(t_game *game, t_ray *ray)
+{
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (game->player.x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - game->player.x) * ray->delta_dist_x;
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (game->player.y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - game->player.y) * ray->delta_dist_y;
+	}
+}
+
+static void	perform_dda(t_game *game, t_ray *ray)
+{
+	while (!ray->hit)
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->map_x += ray->step_x;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->map_y += ray->step_y;
+			ray->side = 1;
+		}
+		if (is_wall(game, ray->map_x, ray->map_y))
+			ray->hit = 1;
+	}
+}
+
+static void	draw_wall_slice(t_game *game, t_ray *ray, int x)
+{
+	int	line_height;
+	int	draw_start;
+	int	draw_end;
+	int	wall_color;
+
+	if (ray->side == 0)
+		ray->perp_wall_dist = ray->side_dist_x - ray->delta_dist_x;
+	else
+		ray->perp_wall_dist = ray->side_dist_y - ray->delta_dist_y;
+	if (ray->perp_wall_dist <= 0.0001)
+		ray->perp_wall_dist = 0.0001;
+	line_height = (int)(WIN_H / ray->perp_wall_dist);
+	draw_start = -line_height / 2 + WIN_H / 2;
+	draw_end = line_height / 2 + WIN_H / 2;
+	wall_color = 0xD2B48C;
+	if (ray->side == 1)
+		wall_color = 0xA28264;
+	draw_vertical_line(game, x, draw_start, draw_end, wall_color);
+}
+
+static void	render_frame(t_game *game)
+{
+	int		x;
+	t_ray	ray;
+
+	draw_background(game);
+	x = 0;
+	while (x < WIN_W)
+	{
+		init_ray(game, &ray, x);
+		set_steps_and_sidedist(game, &ray);
+		perform_dda(game, &ray);
+		draw_wall_slice(game, &ray, x);
+		x++;
+	}
+	mlx_put_image_to_window(game->mlx, game->win, game->img.ptr, 0, 0);
+}
+
+static void	rotate_player(t_game *game, double angle)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = game->player.dir_x;
+	game->player.dir_x = game->player.dir_x * cos(angle)
+		- game->player.dir_y * sin(angle);
+	game->player.dir_y = old_dir_x * sin(angle)
+		+ game->player.dir_y * cos(angle);
+	old_plane_x = game->player.plane_x;
+	game->player.plane_x = game->player.plane_x * cos(angle)
+		- game->player.plane_y * sin(angle);
+	game->player.plane_y = old_plane_x * sin(angle)
+		+ game->player.plane_y * cos(angle);
+}
+
+static void	try_move(t_game *game, double move_x, double move_y)
+{
+	double	next_x;
+	double	next_y;
+
+	next_x = game->player.x + move_x;
+	next_y = game->player.y + move_y;
+	if (!is_wall(game, (int)next_x, (int)game->player.y))
+		game->player.x = next_x;
+	if (!is_wall(game, (int)game->player.x, (int)next_y))
+		game->player.y = next_y;
+}
+
+static void	update_player(t_game *game)
+{
+	if (game->key_w)
+		try_move(game, game->player.dir_x * MOVE_SPEED,
+			game->player.dir_y * MOVE_SPEED);
+	if (game->key_s)
+		try_move(game, -game->player.dir_x * MOVE_SPEED,
+			-game->player.dir_y * MOVE_SPEED);
+	if (game->key_a)
+		try_move(game, -game->player.plane_x * MOVE_SPEED,
+			-game->player.plane_y * MOVE_SPEED);
+	if (game->key_d)
+		try_move(game, game->player.plane_x * MOVE_SPEED,
+			game->player.plane_y * MOVE_SPEED);
+	if (game->key_left)
+		rotate_player(game, -ROT_SPEED);
+	if (game->key_right)
+		rotate_player(game, ROT_SPEED);
+}
+
+static int	loop_hook(t_game *game)
+{
+	update_player(game);
+	render_frame(game);
+	return (0);
+}
+
+static int	close_window(t_game *game)
+{
+	if (game->img.ptr)
+		mlx_destroy_image(game->mlx, game->img.ptr);
+	if (game->win)
+		mlx_destroy_window(game->mlx, game->win);
+	if (game->mlx)
+	{
+		mlx_destroy_display(game->mlx);
+		free(game->mlx);
+	}
 	exit(0);
 	return (0);
 }
 
-static int	key_hook(int keycode, t_app *app)
+static int	key_press(int keycode, t_game *game)
 {
 	if (keycode == ESC)
-		close_window(app);
+		close_window(game);
+	if (keycode == W)
+		game->key_w = 1;
+	if (keycode == A)
+		game->key_a = 1;
+	if (keycode == S)
+		game->key_s = 1;
+	if (keycode == D)
+		game->key_d = 1;
+	if (keycode == LEFT)
+		game->key_left = 1;
+	if (keycode == RIGHT)
+		game->key_right = 1;
 	return (0);
+}
+
+static int	key_release(int keycode, t_game *game)
+{
+	if (keycode == W)
+		game->key_w = 0;
+	if (keycode == A)
+		game->key_a = 0;
+	if (keycode == S)
+		game->key_s = 0;
+	if (keycode == D)
+		game->key_d = 0;
+	if (keycode == LEFT)
+		game->key_left = 0;
+	if (keycode == RIGHT)
+		game->key_right = 0;
+	return (0);
+}
+
+static void	init_map_and_player(t_game *game)
+{
+	game->map.grid = g_map_data;
+	game->map.height = 8;
+	game->map.width = 12;
+	game->player.x = 2.5;
+	game->player.y = 2.5;
+	game->player.dir_x = 1.0;
+	game->player.dir_y = 0.0;
+	game->player.plane_x = 0.0;
+	game->player.plane_y = 0.66;
 }
 
 int	main(void)
 {
-	t_app	app;
-	int	img_w;
-	int	img_h;
+	t_game	game;
 
-	app.mlx = mlx_init();
-	if (!app.mlx)
-		return (1);
-	app.win = mlx_new_window(app.mlx, 1024, 1024, "cub3D image test");
-	if (!app.win)
-		return (1);
-	app.img = mlx_xpm_file_to_image(app.mlx, "textures/texturize_voronoi_55_2048.xpm", &img_w, &img_h);
-	if (!app.img)
-		return (write(2, "Error: cannot load textures/demo.xpm\n", 37), 1);
-	mlx_put_image_to_window(app.mlx, app.win, app.img, (1024 - img_w) / 2,
-		(1024 - img_h) / 2);
-	mlx_hook(app.win, 2, 1L << 0, key_hook, &app);
-	mlx_hook(app.win, 17, 0, close_window, &app);
-	mlx_loop(app.mlx);
+	ft_bzero(&game, sizeof(t_game));
+	init_map_and_player(&game);
+	game.mlx = mlx_init();
+	if (!game.mlx)
+		return (write(2, "Error: mlx_init failed\n", 23), 1);
+	game.win = mlx_new_window(game.mlx, WIN_W, WIN_H, "cub3D - DDA first render");
+	if (!game.win)
+		return (write(2, "Error: mlx_new_window failed\n", 29), 1);
+	game.img.ptr = mlx_new_image(game.mlx, WIN_W, WIN_H);
+	if (!game.img.ptr)
+		return (write(2, "Error: mlx_new_image failed\n", 28), 1);
+	game.img.addr = mlx_get_data_addr(game.img.ptr, &game.img.bpp,
+			&game.img.line_len, &game.img.endian);
+	mlx_hook(game.win, 2, 1L << 0, key_press, &game);
+	mlx_hook(game.win, 3, 1L << 1, key_release, &game);
+	mlx_hook(game.win, 17, 0, close_window, &game);
+	mlx_loop_hook(game.mlx, loop_hook, &game);
+	mlx_loop(game.mlx);
 	return (0);
 }
